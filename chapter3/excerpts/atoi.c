48af736fe177293464022fdaa4e95ea9f6d54d95
Mukto Akash <makash@conestogac.on.ca>
2024-07-09 14:34:25 -0400

    Add IOtestes to atoi.c

--- chapter3/excerpts/atoi.c
+++ chapter3/excerpts/atoi.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <stdio.h>
 
 /* atoi: convert s to integer: version 2 */
 int atoi(char s[])
@@ -13,4 +14,44 @@ int atoi(char s[])
     for(n = 0; isdigit(s[i]); i++)
         n = 10 * n + (s[i] - '0');
     return sign * n;
+}
+
+int main()
+{
+    char s[] = "  -123";
+    printf("%d\n", atoi(s)); // -123
+
+    char s2[] = "  123";
+    printf("%d\n", atoi(s2)); // 123
+
+    char s3[] = "";
+    printf("%d\n", atoi(s3)); // 0
+
+    char s4[] = "  123abc";
+    printf("%d\n", atoi(s4)); // 123
+
+    char s5[] = "  123 456";
+    printf("%d\n", atoi(s5)); // 123
+
+    char s6[] = "-0";
+    printf("%d\n", atoi(s6)); // 0
+
+    char s7[] = "0";
+    printf("%d\n", atoi(s7)); // 0
+
+    char s8[] = "  0";
+    printf("%d\n", atoi(s8)); // 0
+
+    char s9[] = "  0  ";
+    printf("%d\n", atoi(s9)); // 0
+
+    char s10[] = "  0  1";
+    printf("%d\n", atoi(s10)); // 0
+
+    char s11[] = "  1 0 ";
+    printf("%d\n", atoi(s11)); // 1
+
+    char s12[] = " - 1 0  ";
+    printf("%d\n", atoi(s12)); // 0    
+    return 0;
 }
