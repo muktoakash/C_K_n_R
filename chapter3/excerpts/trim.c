9acb6b712d56afac5372db23709e8f8ba26c1fef
Mukto Akash <makash@conestogac.on.ca>
2024-07-09 14:00:38 -0400

    Code trim in Chapter 3

---
+++ chapter3/excerpts/trim.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* trim: reove trailing blanks, tabs, newlines */
+int trim(char s[])
+{
+    int n;
+
+    for(n = strlen(s) - 1; n >= 0; n--)
+        if(s[n] != ' ' && s[n] != '\t' && s[n] != '\n')
+            break;
+    s[n+1] = '\0';
+    return n;
+}
