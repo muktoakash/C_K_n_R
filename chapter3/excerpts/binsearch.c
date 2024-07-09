badb9a5e6ab8c08478bfa2e1e9ba0c3cd0b04244
Mukto Akash <makash@conestogac.on.ca>
2024-07-09 14:05:18 -0400

    Add tests to binsearch.c in Chapter 3

--- chapter3/excerpts/binsearch.c
+++ chapter3/excerpts/binsearch.c
@@ -18,3 +18,21 @@ int binsearch(int x, int v[], int n)
     }
     return -1;  /* no match */
 }
+
+main()
+{
+    int i, n;
+    int v[10];
+
+    for(i = 0; i < 10; ++i)
+        v[i] = i;
+    n = 10;
+
+    printf("%d\n", binsearch(5, v, n)); // 5
+    printf("%d\n", binsearch(10, v, n)); // -1
+    printf("%d\n", binsearch(0, v, n)); // 0
+    printf("%d\n", binsearch(9, v, n)); // 9
+    printf("%d\n", binsearch(1, v, n)); // 1
+    printf("%d\n", binsearch(8, v, n)); // 8
+    return 0;
+}
