0e467eef939874e7de1916ad59cc7b4554d7df2d
Mukto Akash <makash@conestogac.on.ca>
2024-07-31 10:12:06 -0400

    Code atof files in Chapter 4

--- chapter4/excerpts/main.c
+++ chapter4/excerpts/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "get_line.h"
 #include "strindex.h"
+#include "atof.h"
 
 #define MAXLINE 1000 /* maximum input line length */
 
@@ -25,4 +26,17 @@ int test_get_line()
         }
     
     return found;
+}
+
+/* rudimentary calculator */
+int test_atof()
+{
+    double sum, atof(char []);
+    char line[MAXLINE];
+    int get_line(char line[], int max);
+
+    sum = 0;
+    while (get_line(line, MAXLINE) > 0)
+        printf("\t%g\n", sum += atof(line));
+    return 0;
 }
