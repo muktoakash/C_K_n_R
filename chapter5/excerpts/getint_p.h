e5813cd4e4394b1399b27d6cbab5276f1094039c
Mukto Akash <makash@conestogac.on.ca>
2024-11-26 12:49:17 -0500

    Code getint_p

---
+++ chapter5/excerpts/getint_p.h
@@ -0,0 +1,13 @@
+/* interface for getint_p.c */
+
+#ifndef GETINT_P_H
+    #define GETINT_P_H
+    /* getint_p(*pn)
+    
+    Get next integer from input into *pn.
+    Requires: pn is a valid pointer.
+    Side effects: Modifies *pn.
+    Returns: The next character from input.
+    */
+    int getint_p(int *pn);
+#endif
