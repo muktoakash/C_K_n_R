33ed023a56661811e0f972a34288cd097a3e19f5
Mukto Akash <makash@conestogac.on.ca>
2024-12-09 15:23:33 -0500

    Update documentation for Chapter5

--- chapter5/excerpts/qsort_p.h
+++ chapter5/excerpts/qsort_p.h
@@ -5,8 +5,6 @@
 #ifndef QSORT_P_H
     #define QSORT_P_H
 
-    #include "swap_p.h"
-
     /* qsort_p1(v[], left, right)
     
     Sorts v[left]...v[right] into increasing order.
--- chapter5/excerpts/sort_lines.h
+++ chapter5/excerpts/sort_lines.h
@@ -5,14 +5,6 @@
 #ifndef SORT_LINES_H
     #define SORT_LINES_H
 
-    #include <stdio.h>
-    #include <string.h>
-
-    #include "sort_lines.h"
-    #include "alloc_free_p.h"
-    #include "qsort_p.h"
-    #include "../../chapter4/excerpts/get_line.h"
-
     #define MAXLINES 5000 /* max #lines to be sorted */
 
     # define MAXLEN 1000 /* max length of any input line */
