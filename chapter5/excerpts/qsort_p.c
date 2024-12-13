4151ef545a88a8703098a52367c2d3c3bf7b2d1f
Mukto Akash <makash@conestogac.on.ca>
2024-12-12 21:11:04 -0500

    Include qsort v2 in chapter5

--- chapter5/excerpts/qsort_p.c
+++ chapter5/excerpts/qsort_p.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include "swap_p.h"
 #include "qsort_p.h"
+#include "swap_p.h"
 
 /* qsort: sort v[left] ... v[right] into increasing order */
 
@@ -19,4 +20,21 @@ void qsort_p1(void *v[], int left, int right)
     swap_p2(v, left, last);
     qsort_p1(v, left, last-1);
     qsort_p1(v, last+1, right);
+}
+
+void qsort_p2(void *v[], int left, int right, 
+    int (*comp)(void *, void *))
+{
+    int i, last;
+    
+    if (left >= right) /* do nothing if array contains */
+        return;        /* fewer than two elements */
+    swap_p2(v, left, (left + right)/2);
+    last = left;
+    for (i = left+1; i <= right; i++)
+        if ((*comp)(v[i], v[left]) < 0)
+            swap_p2(v, ++last, i);
+    swap_p2(v, left, last);
+    qsort_p2(v, left, last-1, comp);
+    qsort_p2(v, last+1, right, comp);
 }
